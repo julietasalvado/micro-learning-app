70808d00f536c584b43428a085fdea514ae9c863
julietasalvado <julieta.salvado@gmail.com>
2019-09-21 20:18:21 -0300

    Improve article tile (#7)

--- src/main/frontend/Tile.js
+++ src/main/frontend/Tile.js
@@ -4,10 +4,9 @@ import { Card, Icon} from 'semantic-ui-react'
 
 function Tile() {
     return (
-        <Card>
+        <Card color='teal' href="https://dzone.com/articles/everything-react-tutorials-for-beginners-and-exper">
             <Card.Content header='Everything React: Tutorials for Beginners and Experts Alike' />
             <Card.Content description="Released by Facebook in 2013, React has steadily become an industry-standard library for front-end development. Per Stack Overflow's most recent Developer Survey, React sits at the top of the list for both developer's most loved and wanted web library/framework of 2019. This ..."/>
-            <a href="https://dzone.com/articles/everything-react-tutorials-for-beginners-and-exper">https://dzone.com/articles/everything-react-tutorials-for-beginners-and-exper</a>
             <Card.Content extra>
                 <Icon name='file alternate outline' />Article
             </Card.Content>
