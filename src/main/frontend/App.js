ee1a202bf2cdd70c614e45d55e21f2a94feef47c
julietasalvado <julieta.salvado@gmail.com>
2019-09-20 23:40:41 -0300

    Remove spinning logo (#6)

--- src/main/frontend/App.js
+++ src/main/frontend/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tile from './Tile.js'
 import {Container, Header, Tab, Input} from "semantic-ui-react";
@@ -20,10 +19,6 @@ function App() {
 
           <div className="App">
               <Tile />
-              <header className="App-header">
-                  <h1> Microlearning app</h1>
-                  <img src={logo} className="App-logo" alt="logo"/>
-              </header>
           </div>
       </Container>
       </div>
