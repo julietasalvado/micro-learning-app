57fa0dd4d21e71d690a89b17ea968220cadbb070
Julieta Salvadó <julieta.salvado@gmail.com>
2019-09-21 20:47:05 -0300

    Use props for articles

--- src/main/frontend/Tile.js
+++ src/main/frontend/Tile.js
@@ -2,11 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Card, Icon} from 'semantic-ui-react'
 
-function Tile() {
+function Tile(props) {
     return (
-        <Card color='teal' href="https://dzone.com/articles/everything-react-tutorials-for-beginners-and-exper">
-            <Card.Content header='Everything React: Tutorials for Beginners and Experts Alike' />
-            <Card.Content description="Released by Facebook in 2013, React has steadily become an industry-standard library for front-end development. Per Stack Overflow's most recent Developer Survey, React sits at the top of the list for both developer's most loved and wanted web library/framework of 2019. This ..."/>
+        <Card color='teal' href={props.content.url}>
+            <Card.Content header={props.content.header} />
+            <Card.Content description={props.content.body} />
             <Card.Content extra>
                 <Icon name='file alternate outline' />Article
             </Card.Content>
